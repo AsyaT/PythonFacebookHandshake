768dc4b49ce8df553c6420f5097586914781295a
Asya Troyan <asya99@yandex.ru>
2017-11-27 20:57:36 +0500

    refactoring

--- Main.py
+++ Main.py
@@ -3,20 +3,31 @@ from selenium import webdriver
 import DatabaseProvider
 import FacebookActions
 
+def RecurrentFriendsSaver(browser, current_user_id, distination_user_id):
+
+	FacebookActions.VisitProfilePage(browser, current_user_id)
+	current_person = FacebookActions.CreateStartUserOnProfilePage(browser, current_user_id)
+	
+	curent_user_node = DatabaseProvider.CreatePersonNode(current_person)
+	
+	FacebookActions.OpenFriendsTab(browser)
+	f_l = FacebookActions.ListOfFriends(browser)
+	
+	for friend in f_l:
+		friend_node = DatabaseProvider.CreatePersonNode(friend)
+		DatabaseProvider.MakeFriends(curent_user_node,friend_node)
+		if friend.id == distination_user_id:
+			print("Distination user is found!")
+			break;
+		else:
+			#open tab
+			browser.find_element_by_tag_name('body').send_keys(Keys.COMMAND + 't') 
+			RecurrentFriendsSaver(friend.id, distination_user_id)
+
+
 browser = webdriver.Firefox()
 FacebookActions.LoginIntoFacebook(browser)
 
-start_person_id = "100000766204700"
-
-FacebookActions.VisitProfilePage(browser, start_person_id)
-node_1 = DatabaseProvider.CreatePersonNode(FacebookActions.CreateStartUserOnProfilePage(browser, start_person_id))
-
-FacebookActions.OpenFriendsTab(browser)
-
-friends_list = FacebookActions.ListOfFriends(browser)
-for friend in friends_list:
-	friend_node = DatabaseProvider.CreatePersonNode(friend)
-	DatabaseProvider.MakeFriends(node_1,friend_node)
-	
+RecurrentFriendsSaver(browser,"100000766204700","888")
 
 browser.close()
